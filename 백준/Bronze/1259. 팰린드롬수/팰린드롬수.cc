e0cda7d7b682fa1c9cdc61298b9b033a6c84a3c0
suminnii <ks04mm@gmail.com>
2025-02-02 14:04:41 +0900

    [Bronze I] Title: 팰린드롬수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    string s1;
+    
+    while(true){
+        cin>>s1;
+        string s2=s1;
+        reverse(s1.begin(), s1.end());
+        
+        if(s1=="0"){
+            break;
+        }
+        else if(s1==s2){
+            cout<<"yes"<<"\n";
+        }
+        else{
+            cout<<"no"<<"\n";
+        }
+    }
+    
+return 0;
+}
